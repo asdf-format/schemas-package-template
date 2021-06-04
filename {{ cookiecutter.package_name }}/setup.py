40ff419b0d1975f60fd6a70069f239f7e1a959cf
Ed Slavich <eslavich@stsci.edu>
2021-06-04 13:37:57 -0400

    Fix package data issues

--- {{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.package_name }}/setup.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-from setuptools import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-setup()
+
+packages = find_packages(where="src")
+packages.append("{{ cookiecutter.module_name }}.resources")
+
+package_dir = {
+    "": "src",
+    "{{ cookiecutter.module_name }}.resources": "resources",
+}
+
+package_data = {
+    "{{ cookiecutter.module_name }}.resources": ["*.yaml", "**/*.yaml", "**/**/*.yaml"],
+}
+
+setup(
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+)
