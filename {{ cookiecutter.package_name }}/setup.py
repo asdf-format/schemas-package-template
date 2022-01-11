ec3a827a9eff3e1bc9d22b14432a2990565d3d0a
William Jamieson <wjamieson@stsci.edu>
2022-01-11 14:49:23 -0500

    Added recursive package data add.

--- {{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.package_name }}/setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
 
@@ -10,8 +11,18 @@ package_dir = {
     "{{ cookiecutter.module_name }}.resources": "resources",
 }
 
+
+def package_yaml_files(directory):
+    paths = []
+    for path, _, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith(".yaml"):
+                paths.append(os.path.join("..", path, filename))
+    return paths
+
+
 package_data = {
-    "{{ cookiecutter.module_name }}.resources": ["*.yaml", "**/*.yaml", "**/**/*.yaml"],
+    "{{ cookiecutter.module_name }}.resources": package_yaml_files("resources"),
 }
 
 setup(
